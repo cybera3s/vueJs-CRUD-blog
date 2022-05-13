41b7a27ecd4c7c77f7cda9afdadd39feaab332c9
ario <cybera.3s@gmail.com>
2022-05-14 01:07:11 +0430

    add profile view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>  
+    <router-link to="/about">About</router-link> 
+    <router-link to="/profile">Profile</router-link>
   </nav>
   <router-view/>
 </template>
@@ -22,6 +23,8 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  margin-right: 10px;
+  text-decoration: none;
 }
 
 nav a.router-link-exact-active {
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import Profile from '../views/Profile.vue'
+
 
 const routes = [
   {
@@ -10,11 +13,13 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: AboutView,
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile,
+  },
 ]
 
 const router = createRouter({
