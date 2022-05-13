7df69cdfa0bd1fdb5edceeecfe77a416df053c46
ario <cybera.3s@gmail.com>
2022-05-14 01:38:25 +0430

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>  
-    <router-link to="/about">About</router-link> 
-    <router-link to="/profile">Profile</router-link>
+  
+  <nav id="nav" class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">Aiva</router-link>  
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link active" to="/">Home</router-link>  
+          </li>
+
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link> 
+
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/profile">Profile</router-link>
+
+          </li>
+          
+
+
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-light" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
   </nav>
-  <router-view/>
+  <router-view class="container " />
 </template>
 
 <style>
@@ -16,9 +43,7 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
+
 
 nav a {
   font-weight: bold;
@@ -28,6 +53,6 @@ nav a {
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: black;
 }
 </style>
