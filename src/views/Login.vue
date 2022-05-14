a12cee372b57e7a59e3ac13a9112d5b701daedaf
ario <cybera.3s@gmail.com>
2022-05-14 22:53:18 +0430

    add login validation

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,12 +9,32 @@
 
               <div class="mb-3">
                 <label for="username" class="form-label">Username</label>
-                <input type="text" class="form-control" id="username" v-model="username">
+                <input 
+                    type="text" 
+                    class="form-control" id="username" 
+                    v-model="username"
+                    :class="{
+                      'is-invalid': usernameE === true,
+                      'is-valid': usernameE === false
+                    }">
+                    <div class="invalid-feedback" v-if="usernameE">
+                      {{usernameEM}}
+                    </div>
               </div>
 
               <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" v-model="password">
+                <input 
+                type="password" 
+                class="form-control" id="password" 
+                v-model="password"
+                :class="{
+                  'is-invalid': passwordE === true,
+                  'is-valid': passwordE === true
+                }">
+                 <div class="invalid-feedback" v-if="passwordE">
+                      {{passwordEM}}
+                    </div>
               </div>
 
               <button type="submit" class="btn btn-primary w-100">Login</button>
@@ -35,14 +55,49 @@
         return {
           username: '',
           password: '',
+          usernameE: null,
+          passwordE: null,
+          usernameEM: null,
+          passwordEM: null,
         }
     },
     methods: {
       doLogin(){
-        console.log(this.username)
-        console.log(this.password)
-        // this.$store.commit("login", "abc123")
-        // this.$router.push("profile")
+        let access = true
+
+        if (this.username.length < 5){
+          this.usernameE = true
+          access = false
+          if (this.username.length == 0){
+            this.usernameEM = "Username required!"
+
+          } else {
+              this.usernameEM = "Username must be at least 5 characters"            
+          }
+        } else {
+          this.usernameE = false
+          this.usernameEM = ''
+        }
+
+        if (this.password.length < 6){
+          this.passwordE = true
+          access = false
+          if (this.password.length == 0){
+            this.passwordEM = "Password required!"
+
+          } else {
+              this.passwordEM = "Password must be at least 5 characters"            
+          }
+        } else {
+          this.passwordE = false
+          this.passwordEM = ''
+        }
+
+        if (access){
+            this.$store.commit("login", `${this.username}:${this.password}`)
+            this.$router.push("profile")
+        }
+        
       }
     }
   }
