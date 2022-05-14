ea4f6c2db023bc0e362f09055c04f93ef37a7c4e
ario <cybera.3s@gmail.com>
2022-05-14 20:13:25 +0430

    add login page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="Home">
+      <h1>Home</h1>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <hr>
+      </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+ 
 }
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="Login">
-    <h1>This is an Login page</h1>
-    <button @click="doLogin" class="btn btn-success">Login</button>
+
+    <div class="row">
+      <div class="col-11 col-sm-10 col-md-8 col-lg-6 col-xl-4 mx-auto border shadow rounded p-4 mt-3">
+          <h1  class="text-center text-shadow">Login page</h1>
+          <hr>
+          <form @submit.prevent="doLogin">
+
+              <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="username" v-model="username">
+              </div>
+
+              <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" v-model="password">
+              </div>
+
+              <button type="submit" class="btn btn-primary w-100">Login</button>
+
+          </form>
+      </div>
+    </div>
+    
+
   </div>
-  <!-- {{$store}} -->
-  {{$router}}
+
 </template>
 
 <script>
   export default{
     name:'Login',
+    data(){
+        return {
+          username: '',
+          password: '',
+        }
+    },
     methods: {
       doLogin(){
-        this.$store.commit("login", "abc123")
-        this.$router.push("profile")
+        console.log(this.username)
+        console.log(this.password)
+        // this.$store.commit("login", "abc123")
+        // this.$router.push("profile")
       }
     }
   }
