5f4fc105935b35aa31ddc6a056cb2e3f9f80c1e4
ario <cybera.3s@gmail.com>
2022-05-14 14:40:21 +0430

    remove active class from nav links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,7 +10,7 @@
 			      	<div class="collapse navbar-collapse" id="navbarColor01">
 				        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 					          <li class="nav-item">
-					            <router-link class="nav-link active" to="/">Home</router-link>  
+					            <router-link class="nav-link" to="/">Home</router-link>  
 					          </li>
 
 					          <li class="nav-item">
