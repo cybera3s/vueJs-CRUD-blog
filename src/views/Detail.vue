4a73129619789072c328f3f9c02f362ecc9e9950
ario <cybera.3s@gmail.com>
2022-05-15 00:24:13 +0430

    complete detail view

---
+++ src/views/Detail.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="Detail">
+    
+      <!-- Articles -->
+      <article >
+
+        <h3>{{article.title}}</h3>
+        <div>{{article.content}}</div>
+
+      </article>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Detail',
+  data (){
+    let articles = localStorage.getItem("articles")
+    articles = JSON.parse(articles)
+
+    let article = articles.find(
+        article => article.slug == this.$route.params.slug
+    )
+
+    return {
+      articles: articles,
+      article: article,
+    }
+  }
+ 
+}
+</script>
