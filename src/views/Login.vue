6e844dfe7015336c0d0c13ed2f7ad6448ad92c29
ario <cybera.3s@gmail.com>
2022-05-14 18:28:37 +0430

    add login view

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Login">
+    <h1>This is an Login page</h1>
+    <button @click="doLogin" class="btn btn-success">Login</button>
+  </div>
+</template>
+
+
+<script>
+  export default{
+    name:'Login',
+    methods: {
+      doLogin(){
+        this.$store.commit("login", 'abc123')
+      }
+    }
+  }
+</script>
