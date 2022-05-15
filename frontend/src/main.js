99bdef1ffa3491ed19df280dd5800b87b04b37b6
ario <cybera.3s@gmail.com>
2022-05-15 17:37:10 +0430

    import requirement

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -6,5 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import 'jquery/src/jquery.js'
 import 'popper.js/dist/popper.min.js'
 import "bootstrap/dist/js/bootstrap.min.js"
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
 
 createApp(App).use(store).use(router).mount('#app')
