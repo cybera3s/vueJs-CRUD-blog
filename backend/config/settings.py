c989c5bce0c3d5976eb0567e1c2be038dccb8529
ario <cybera.3s@gmail.com>
2022-05-20 23:52:29 +0430

    refactor CORS setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -144,10 +144,14 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-]
+
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8080",
+#     "http://127.0.0.1:8080",
+#     "http://127.0.0.1:8081",
+#     "http://localhost:3000",
+# ]
 
 """
  CORS_ALLOWED_ORIGIN_REGEXES = [
