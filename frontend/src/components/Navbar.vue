b93e38aa80ba8ae06fb01de0170b553a647aa5f0
ario <cybera.3s@gmail.com>
2022-07-03 18:19:19 +0430

    add doLogout method

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -56,7 +56,30 @@
 </template>
 
 <script>
-	export default {
+	import axios from "axios";
+
+  export default {
 		name: 'Navbar',
+    methods: {
+      doLogout(){
+
+
+            axios
+              .post('/api/auth/token/logout/')
+              .then(response =>  {
+
+                    this.$store.commit("logout")
+                    this.$router.push("login")
+
+              })
+              .catch(error => {
+                  console.log(error.response.data)
+                  this.logoutError = error.response.data
+              })
+
+
+
+      }
+    }
 	}
 </script>
